017f587ac8c914a7a72aaa39ae7d7fa1796efd88
Dave Hurley <djmhur@gmail.com>
2024-07-22 03:27:27 +0100

    pass 4th stage

--- app/main.py
+++ app/main.py
@@ -10,7 +10,7 @@ def main():
         command = input()
         if command.startswith("exit"):
             args = command.split()
-            quit(int(args[1])
+            quit(int(args[1]))
         else:
             sys.stdout.write(command + ": command not found\n")
 
