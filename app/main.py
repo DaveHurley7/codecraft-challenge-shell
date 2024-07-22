7d05634c64fb8418c7bb5ba03b0dec005f7052ad
Dave Hurley <djmhur@gmail.com>
2024-07-22 03:08:55 +0100

    pass 3rd stage

--- app/main.py
+++ app/main.py
@@ -3,11 +3,10 @@ import sys
 
 def main():
     # Uncomment this block to pass the first stage
-    sys.stdout.write("$ ")
-    sys.stdout.flush()
-
-    # Wait for user input
     while True:
+        sys.stdout.write("$ ")
+        sys.stdout.flush()
+    # Wait for user input
         command = input()
         sys.stdout.write(command + ": command not found\n")
 
