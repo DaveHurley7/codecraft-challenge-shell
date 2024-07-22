09528bbefc723438f18cba5e9bc0c52d754a046b
Dave Hurley <djmhur@gmail.com>
2024-07-22 21:42:18 +0100

    pass 7th stage

--- app/main.py
+++ app/main.py
@@ -10,7 +10,7 @@ def is_exec(command):
             dir_files = os.listdir(pdir)
             for file in dir_files:
                 if file == command:
-                    print(command)
+                    print("DIR:",pdir)
                     print(os.absname(command))
                     return os.absname(command)
     print("PATH NOT FOUND")
